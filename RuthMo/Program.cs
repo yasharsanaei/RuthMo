21ea32712ed65f7bd337d8d75923b4dde8e45558
yasharsanaei <yasharsanayee@gmail.com>
2025-02-17 19:25:57 +0330

    create user admin

--- RuthMo/Program.cs
+++ RuthMo/Program.cs
@@ -1,18 +1,27 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using RuthMo.Models;
+using Swashbuckle.AspNetCore.Filters;
+using static Microsoft.AspNetCore.Builder.WebApplication;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("RuthMoPostgres");
 var corsPolicyName = "RuthMoCors";
 
-// Add DbContext
-builder.Services.AddDbContext<MotivationContext>(options =>
-    options.UseNpgsql(connectionString));
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(optiosn =>
+{
+    optiosn.AddSecurityDefinition("oath2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    optiosn.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 
 builder.Services.AddCors(options =>
 {
@@ -26,7 +35,41 @@ builder.Services.AddCors(options =>
         });
 });
 
+builder.Services.AddDbContext<MotivationContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<MotivationContext>();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    string[] roles = ["Admin", "User"];
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+
+    // Optional: Create an admin user
+    const string adminEmail = "admin@ruthmo.com";
+    var adminUser = await userManager.FindByEmailAsync(adminEmail);
+
+    if (adminUser == null)
+    {
+        adminUser = new IdentityUser { UserName = adminEmail, Email = adminEmail };
+        await userManager.CreateAsync(adminUser, "Admin@123"); // Change this to a secure password
+        await userManager.AddToRoleAsync(adminUser, "Admin");
+    }
+}
 
 // Middleware
 if (app.Environment.IsDevelopment())
@@ -38,6 +81,8 @@ else
 {
     app.UseHsts();
 }
+
+app.MapIdentityApi<IdentityUser>();
 
 app.UseHttpsRedirection();
 app.UseCors(corsPolicyName);
